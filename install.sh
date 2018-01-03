89d7f829cbdadb9c26116499909f65a350382536
Junichi Motohisa <motohisa@ist.hokudai.ac.jp>
2018-01-04 08:49:05 +0900

    install.sh updated

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ ln -s `pwd`/dotzshd ~/.zsh.d
 ln -s `pwd`/jmdot-emacs-d ~/.emacs.d
 echo 'export ZDOTDIR=$HOME/.zsh.d' > ~/.zshenv
 
+# ln -s `pwd`/.valgrindrc ~/.valgrindrc
+# ln -s `pwd`/valgrind/jm.sup ~/local/share/valgrind/jm.sup
