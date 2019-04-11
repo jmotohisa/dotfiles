c54e0fd08108ac1bdb64031de3d0dc51fb365b78
Junichi Motohisa <motohisa@lsilab-vt.ist.hokudai.ac.jp>
2019-04-11 11:43:14 +0900

    updating for explanation for install

--- install.sh
+++ install.sh
@@ -10,9 +10,13 @@ echo 'export ZDOTDIR=$HOME/.zsh.d' > ~/.zshenv
 ## edit dotzshd/.zshrc_local
 
 ## for emacs
-# cd ~/.emacs.d; mkdir lisp; mkdir site-start.d
+# cd ~/.emacs.d; mkdir lisp; mkdir site-start.d; mkdir local-lisp
 ## install ddskk:
+# wget 
+# cd ddskk-XX.X; cd dic; wget http://openlab.ring.gr.jp/skk/skk/dic/SKK-JISYO.L
+# 
 ## igor-mode
+# cd ~/.emacs.d/lisp
 # git clone https://github.com/yamad/igor-mode.git
 ## FreeFem mode
 # git clone https://github.com/rrgalvan/freefem-mode.git
