f9fe16fb875f3f1bcbf1073eeb1d03ab45c7492a
Junichi Motohisa <motohisa@ist.hokudai.ac.jp>
2019-04-11 11:46:27 +0900

    template.v updated

--- jmdot-emacs-d/lisp/insert/template.v
+++ jmdot-emacs-d/lisp/insert/template.v
@@ -1,5 +1,6 @@
+`timescale 1ns / 1ps
 //--------------------------------------------------------------------------------
-//  %file% - Time-stamp: <Sat Dec 22 13:40:25 JST 2018>
+//  %file% - Time-stamp: <Tue Feb 26 19:08:43 JST 2019>
 //
 //   Copyright (c) %year%  %id% (%name%)  <%mail%>
 //
@@ -20,11 +21,14 @@
 //
 //--------------------------------------------------------------------------------
 
-module modulename #(parameter WIDTH= 8)
+module modulename 
+  #(
+	parameter WIDTH= 8
+	)
    (
     input wire 			   clk,
     input wire 			   rst,
-	output wire 		   out1,
+	output reg 			   out1,
 	output reg [WIDTH-1:0] out2
     );
    
